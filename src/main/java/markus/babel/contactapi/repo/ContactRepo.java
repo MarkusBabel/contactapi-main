3198cdfb99279b74c378b217b9b57a967d5f71fb
Markus <127338769+MarkusBabel@users.noreply.github.com>
2024-01-19 19:10:04 +0200

    publishing

---
+++ src/main/java/markus/babel/contactapi/repo/ContactRepo.java
@@ -0,0 +1,12 @@
+package markus.babel.contactapi.repo;
+
+import markus.babel.contactapi.domain.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepo extends JpaRepository<Contact, String> {
+    Optional<Contact> findById(String id);
+}
---
+++ src/test/java/markus/babel/contactapi/FileuploadanddownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package markus.babel.contactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadanddownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
